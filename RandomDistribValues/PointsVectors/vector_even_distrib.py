7408fdac22ac7fdfad5c435054efa1c1de27635c
StD <sdmitrysci@gmail.com>
2024-03-20 14:59:09 +0400

    added description

--- RandomDistribValues/PointsVectors/vector_even_distrib.py
+++ RandomDistribValues/PointsVectors/vector_even_distrib.py
@@ -4,10 +4,13 @@ import math as m
 
 def vector_even_distrib(M: list, r: float) -> list:
     """
+    Generates a random vector with a specified length r, originating from a given point M, and with an evenly
+    distributed direction on a sphere
 
-    :param r: length of vector
+    :param r: The desired length of the random vector
     :param M: M = [x_ini, y_ini, z_ini] - initial point of vector
-    :return: R = [x_ini, y_ini, z_ini, x, y, z] - random vector with even distribution over sphere
+    :return: R = [x_ini, y_ini, z_ini, x, y, z] - Its direction is randomly distributed on a sphere, ensuring
+        an even distribution of vectors on the spherical surface
     """
 
     phi, cos_t, sin_t = rotation_even_distrib()
