980ac859bd0dafb2c41932d561a5ce0b688e1c31
StD <sdmitrysci@gmail.com>
2024-03-20 14:56:32 +0400

    added description

--- RandomDistribValues/PointsVectors/coord_sys_even_distrib.py
+++ RandomDistribValues/PointsVectors/coord_sys_even_distrib.py
@@ -4,9 +4,13 @@ import math as m
 
 def coord_sys_even_distrib(M: list):
     """
+    Generates a random coordinate system with an evenly distributed set of orthogonal unit vectors X, Y, and Z,
+    originating from a specified point M.
 
     :param M: M = [x_ini, y_ini, z_ini] - origin of coordinate system
     :return: G = [X, Y, Z]. X = [Xx, Xy, Xz], Y = [Yx, Yy, Yz], Z = [Zx, Zy, Zz]
+        A list containing the three orthogonal unit vectors X, Y, and Z, representing the new coordinate system.
+        Each vector is represented as a list of its three components
     """
 
     X = vector_even_distrib(M, 1)
