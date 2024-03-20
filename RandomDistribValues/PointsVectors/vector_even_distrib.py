2d64bece1df6ca42bd4d9c0ddcd7c0ea762e020e
StD <sdmitrysci@gmail.com>
2024-03-20 14:47:06 +0400

    added randomizer for a vector

---
+++ RandomDistribValues/PointsVectors/vector_even_distrib.py
@@ -0,0 +1,22 @@
+from MathTools.RandomDistribValues.PointsVectors.rotation_even_distrib import rotation_even_distrib
+import math as m
+
+
+def random_vector_even_distribution(M: list, r: float) -> list:
+    """
+
+    :param r: length of vector
+    :param M: M = [x_ini, y_ini, z_ini] - initial point of vector
+    :return: R = [x_ini, y_ini, z_ini, x, y, z] - random vector with even distribution over sphere
+    """
+
+    phi, cos_t, sin_t = rotation_even_distrib()
+    sin_phi = m.sin(phi)
+    cos_phi = m.cos(phi)
+    rx = r * sin_t * cos_phi
+    ry = r * sin_t * sin_phi
+    rz = r * cos_t
+    R = [rx, ry, rz]
+    R = M + R
+
+    return R
