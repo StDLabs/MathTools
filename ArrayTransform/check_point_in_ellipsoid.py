c50191f9f4363852865557f581052a6180bcc333
StD <sdmitrysci@gmail.com>
2024-05-08 15:46:17 +0400

    Adjust description

--- ArrayTransform/check_point_in_ellipsoid.py
+++ ArrayTransform/check_point_in_ellipsoid.py
@@ -4,9 +4,9 @@ import numpy as np
 def check_point_in_ellipsoid(R: list, L: list) -> bool:
     """
 
-    :param R: R = [x, y, z] - some point
-    :param L: L = [a, b, c] - parameters of ellipsoid a > b > c
-    :return: True/False
+    :param R: R = [x, y, z] - Any point in 3D Cartesian coordinate system
+    :param L: L = [a, b, c] - Linear measures for an ellipsoid according to its canonical equation
+    :return: key = True/False
     """
 
     key = (np.array([(R[j] / L[j]) ** 2 for j in range(0, 3)])).sum() <= 1
