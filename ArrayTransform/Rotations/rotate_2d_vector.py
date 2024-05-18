7e8305c2fd9f2bd5f6ed8001c78b97a24c660647
StD <sdmitrysci@gmail.com>
2024-05-18 19:33:03 +0400

    Improving input values and description. Added good example

--- ArrayTransform/Rotations/rotate_2d_vector.py
+++ ArrayTransform/Rotations/rotate_2d_vector.py
@@ -1,42 +1,64 @@
 import numpy as np
+import math as m
 from MathTools.ArrayTransform.Rotations.rotate_simple_2d_vector import rotate_simple_2d_vector
+from MathTools.PlotVisualize.PlaneMapping.plot_vector_field_2d import plot_vector_field_2d
 
 
-def rotate_2d_vector(R0: list, phi: float, rotate_ini: bool, relative: bool) -> list:
+def rotate_2d_vector(A: list, phi: float, rotate_rad: bool, relative: bool) -> list:
     """
     Rotates a 2D vector by a specified polar angle, with the option to rotate around an initial point
     and handle relative or absolute coordinate systems
 
-    :param R0: A list representing the initial 2D vector. It can have two formats:
-        R0 = [x0, y0] (Coordinates of the vector) or
-        R0 = [x0_ini, y0_ini, x0, y0] (Coordinates of the initial point and the vector)
-    :param phi: phi - The polar angle (in radians) by which the vector should be rotated
-    :param rotate_ini: If True, the initial point [x0_ini, y0_ini] will also be rotated
-    :param relative: If True, the rotation of [x0, y0] is performed relative to the initial point [x0_ini, y0_ini].
-        If False, the rotation is performed in the absolute coordinate system
-    :return: R representing the rotated 2D vector. The format of R depends on the input format of R0.
-        R = [x, y] or R = [x_ini, x_ini, x, y]
+    :param A: A list representing the initial 2D vector. It can have two formats:
+        A = [Ax, Ay] (vector projections) or
+        A(R) = [x, y, Ax, Ay] (Coordinates of an observation point R = [x, y] (radius-vector)
+        and vector projections A = [Ax, Ay] (main vector value))
+    :param phi: phi - The polar angle (in radians) by which the vector is to be rotated
+    :param rotate_rad: If True, the radius-vector R = [x, y] will also be rotated around z-axis
+    :param relative: If True, the rotation of the main vector A = [Ax, Ay] is performed relatively
+        to the observation point R = [x, y]. If False, the rotation is performed in the absolute coordinate system
+    :return: Ar representing the rotated 2D vector. The format of Ar depends on the input format of A.
+        Ar = [Arx, Ary] or Ar(Rr) = [xr, yr, Arx, Ary]
     """
 
-    n: bool = len(R0) == 4
+    n: bool = len(A) == 4
 
     if n:
-        R = [R0[2], R0[3]]
-        R_ini = [R0[0], R0[1]]
+        Ar = [A[2], A[3]]
+        R = [A[0], A[1]]
     else:
-        R = [R0[0], R0[1]]
+        Ar = [A[0], A[1]]
 
     if not n:
-        R = rotate_simple_2d_vector(R, phi)
+        Ar = rotate_simple_2d_vector(Ar, phi)
     else:
+        Ar = np.array(Ar)
         R = np.array(R)
-        R_ini = np.array(R_ini)
         if not relative:
-            R = np.array(rotate_simple_2d_vector(list(R - R_ini), phi)) + R_ini
+            Ar = np.array(rotate_simple_2d_vector(list(Ar - R), phi)) + R
         else:
+            Ar = np.array(rotate_simple_2d_vector(list(Ar), phi))
+        if rotate_rad:
             R = np.array(rotate_simple_2d_vector(list(R), phi))
-        if rotate_ini:
-            R_ini = np.array(rotate_simple_2d_vector(list(R_ini), phi))
-        R = list(R_ini) + list(R)
+        Ar = list(R) + list(Ar)
 
-    return R
+    return Ar
+
+
+def rotate_2d_vector_example():
+
+    R = [2, 2]
+    A = [-0.65, 0.75]
+    G = [[R[0], R[1], A[0], A[1]]]
+    N = 100
+    An = np.linalg.norm(A)
+    h = An / N
+    phi = m.pi / (N / 2)
+    for i in range(0, N):
+        Ar = rotate_2d_vector(G[i], phi, False, True)
+        An = np.linalg.norm([Ar[2], Ar[3]])
+        Ar = [Ar[0], Ar[1], Ar[2] * (1 + h / An), Ar[3] * (1 + h / An)]
+        G.append(Ar)
+    plot_vector_field_2d(G, 2, True, False, True)
+
+    return
