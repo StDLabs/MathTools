5de4f615ed38851ade3de16d70f8590bb52f5ac4
StD <sdmitrysci@gmail.com>
2024-07-03 13:59:28 +0400

    Added plot_scalar_field_contour_map function that generates and displays a contour map of a scalar field over a 2D grid section defined by a plane in 3D space. Added descriptions and features.

--- PlotVisualize/PlaneMapping/plot_scalar_field_contour_map.py
+++ PlotVisualize/PlaneMapping/plot_scalar_field_contour_map.py
@@ -1,8 +1,30 @@
 from MathTools.ArrayTransform.AnalytGeom.plane_type import plane_type
+from MathTools.log_files import check_log_folder
 import matplotlib.pyplot as plt
+import datetime
 
 
-def plot_scalar_field_contour_map(M1, M2, F, LN, Title, Pl, P, Save):
+def plot_scalar_field_contour_map(M1, M2, F, LN, Title, Pl, P, Save, layout):
+    """
+    Description
+        This function generates and displays a contour map of a scalar field over a 2D grid section defined by a plane
+        in 3D space. It can visualize the field with different color schemes and optionally save the plot.
+        Supports different visualization styles ("DarkBlue" and "White"), with easy change of aspects and colors.
+        Labels the axes based on the plane orientation and includes a detailed title. The function chooses the correct
+        plane orientation and adjusts the plot labels accordingly.
+
+    :param M1: The x-coordinates (or y, z depending on the plane orientation) of the 2D grid points
+    :param M2: The y-coordinates (or x, z depending on the plane orientation) of the 2D grid points
+    :param F: A 2D array representing the scalar field values at the grid points defined by M1 and M2.
+    :param LN: The number of contour levels to draw
+    :param Title: The title of the contour plot, often describing the field being visualized
+    :param Pl: Pl = [A, B, C, D]. Parameters of the plane in the form Ax+By+Cz+D=0
+    :param P: P = [Px, Py, Pz] - defines the area bounds {{-Px,Px},{-Py,Py},{-Pz,Pz}} for calculations
+    :param Save: Flag to determine whether the plot should be saved to a file (True) or displayed (False).
+    :param layout: Specifies the color scheme and style for the contour plot.
+        Supported values are 'DarkBlue' and 'White' with labels.
+    :return:
+    """
 
     Axis = ('XYZ')
     plane_info = plane_type(Pl)
@@ -11,8 +33,14 @@ def plot_scalar_field_contour_map(M1, M2, F, LN, Title, Pl, P, Save):
     i2 = plane_info[3][1]
     U = -Pl[3] / Pl[s]
 
-    plt.figure(1, figsize=(6, 4))
-    CS = plt.contourf(M1, M2, F, LN, cmap=plt.cm.gray)
+    plt.figure(1, figsize=(6, 5))
+    if layout == 'DarkBlue':
+        CS = plt.contourf(M1, M2, F, LN, cmap=plt.cm.bone)  # YlGnBu
+    if layout == 'White':
+        CS = plt.contour(M1, M2, F, LN, alpha=1, cmap=plt.cm.viridis, linewidths=0.8)
+        CL = plt.clabel(CS, CS.levels, inline=True, fontsize=7, rightside_up=True, inline_spacing=5)
+        for label in CL:
+            label.set_rotation(0)
     CB = plt.colorbar(CS, shrink=0.8, label=Title)
     CB.formatter.set_powerlimits((0, 0))
     CB.update_ticks()
@@ -20,11 +48,16 @@ def plot_scalar_field_contour_map(M1, M2, F, LN, Title, Pl, P, Save):
     plt.ylabel('Axis ' + str(Axis[i2]) + ': [' + str(-P[i2]) + ', ' + str(P[i2]) + ' ]')
     plt.title('Field section ' + Axis[s] + ' = ' + str(U))
     # plt.axes().set_aspect('equal')
-    plt.grid(True)
+    # plt.grid(True, color='0.8')
     if s == 2:
         plt.gca().invert_yaxis()
     if Save:
-        vv = 1
+        now = datetime.datetime.now()
+        now = datetime.date(now.year, now.month, now.day)
+        N, P = check_log_folder()
+        plt.savefig(
+            'Data/Chapter ' + str(now.year) + '.' + str(now.month) + '.' + str(now.day) + '/' + str(N) + '.' + str(
+                P + 1) + '.png')
     else:
         plt.show()
 
--- PointGenerators/SpatialFields/scalar_field_section.py
+++ PointGenerators/SpatialFields/scalar_field_section.py
@@ -5,17 +5,29 @@ import numpy as np
 
 def scalar_field_section(Pl, P, Npl, G, Q, A, function):
     """
+    Description
+        This function calculates the scalar field values on a 2D rectangular grid section
+        defined by a plane in 3D space, produced by a source (for example, by a set of point charges).
+        It supports calculations only for horizontal and vertical planes. Constructs a 2D grid based on user-defined
+        nodal points and the area of interest. Evaluates the effect of point charges on the grid points using
+        a user-defined field-producing function (for example classical Coulomb's field).
 
-    :param Pl: Pl = [A, B, C, D] - parameters of a plane Ax+By+Cz+D=0 in space
-        (supports only horizontal and vertical planes)
-    :param P: P = [Px, Py, Pz] - defines the area {{-Px,Px},{-Py,Py},{-Pz,Pz}} for calculations
-    :param Npl: Npl = [N1, N2] - number of nodal points for 2D rectangular grid
-    :param G: G = [R0, R1, R2, ...], Ri = [Rix, Riy, Riz] - set of points Ri where point charges are located
-    :param Q: Q = [Q0, Q1, Q2, ...], Qi - magnitude of each point charge
+    :param Pl: Pl = [A, B, C, D] - parameters of a plane Ax+By+Cz+D=0 in space. The function supports calculations only
+        for horizontal (parallel to the XY plane) and vertical planes (parallel to the XZ or YZ planes).
+    :param P: P = [Px, Py, Pz] - defines the area bounds {{-Px,Px},{-Py,Py},{-Pz,Pz}} for calculations
+    :param Npl: Npl = [N1, N2] - number of nodal points for 2D rectangular grid. This defines the grid resolution
+    :param G: G = [R0, R1, R2, ...] - List of point charge locations Ri = [Rix, Riy, Riz]
+    :param Q: Q = [Q0, Q1, Q2, ...], Qi - magnitudes point charges
     :param A: A = [A0, A1, A2, ...] - a list of all parameters needed for chosen type of the field producing function
     :param function: name of supported function (see point_charges_field_calc)
-    :return: GF =
+    :return: M1, M2, F
+        M1 - the x-coordinates (or y, z depending on the plane orientation) of the 2D grid points along
+        the first dimension
+        M2 - the y-coordinates (or x, z depending on the plane orientation) of the 2D grid points along
+        the second dimension.
+        F - A 2D array representing the scalar field values at the grid points defined by M1 and M2.
     """
+
     plane_info = plane_type(Pl)
     if plane_info[2] == 2:
         s = plane_info[1][0]
